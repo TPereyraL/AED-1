083110e21c76cb258947d2bfb45f7de5fb0b8948
TPereyraL <tomaspereyralamas@gmail.com>
2023-05-27 19:39:52 -0300

    Terminado, 80% en el 5 y 90% en el 4, corregir

--- CMS2/Ejercicio 5.py
+++ CMS2/Ejercicio 5.py
@@ -9,31 +9,42 @@ from typing import Tuple
 def input_a_tupla(vuelos: str) -> List[Tuple[str, str]]:
     return [tuple(vuelo.split(',')) for vuelo in vuelos.split()]
 
-def vuelo_del_origen(origen: str,vuelos: List[Tuple[str, str]]) -> Tuple[str,str]:
-    res = ('no', 'no')
-    for i in vuelos:
-        if origen == i[0]:
-            res = i
+def vuelo_origen (origen: str, vuelos: List[Tuple[str, str]]) -> Tuple[bool, Tuple[str, str]]:
+    res: bool = False
+    vueloDeOrigen: Tuple[str, str] = ('0','0')
+    for vuelo in vuelos:
+        if vuelo[0] == origen:
+            vueloDeOrigen = vuelo
+            res = True
+    return (res, vueloDeOrigen)
 
-    return res
 
+def ruta (origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
+    ruta: List[Tuple[str, str]] = []
+    while vuelo_origen(origen, vuelos)[0]:
+        ruta.append(vuelo_origen(origen, vuelos)[1])
+        if ruta[len(ruta) - 1][1] != destino:
+            origen = vuelo_origen(origen, vuelos)[1][1]
+        
+        else:
+            origen = 'none'
+
+    if ruta[len(ruta) - 1][1] != destino:
+        ruta = []
+    return ruta
+
+   
 
 
 def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> int :
-    res: int = -1
-    nro_vuelos: int = 0
-    vuelo = vuelo_del_origen(origen,vuelos)
-    while nro_vuelos <= len(vuelos) and vuelo[1] != destino:
-        if vuelo[1] != destino:
-            vuelo = vuelo_del_origen(vuelo[1],vuelos)
-            nro_vuelos += 1
-        
-        else:
-            res = nro_vuelos + 1
-
+    if len(ruta(origen, destino, vuelos)) == 0:
+        res = -1
+    else:
+        res = len(ruta(origen, destino, vuelos))
     return res
 
-origen = input('Ingrese el origen: ')
-destino = input('Ingrese el destino: ')
-vuelos = input_a_tupla(input('Ingrese los vuelos: '))
+origen = 'Roma'
+destino = 'Madrid'
+vuelos = input_a_tupla('Paris,Roma Barcelona,Bangladesh Bangladesh,Estambul Estambul,Madrid Madrid,Paris Roma,Barcelona')
+print ('La ruta sera:', ruta(origen,destino,vuelos))
 print ('La cantidad de vuelos sera:',sePuedeLlegar(origen,destino,vuelos))
--- CMS2/Entregables/sePuedeLlegar.py
+++ CMS2/Entregables/sePuedeLlegar.py
@@ -5,9 +5,39 @@ from typing import Tuple
 # l: List[int]  <--Este es un ejemplo para una lista de enteros.
 # t: Tuple[str,str]  <--Este es un ejemplo para una tupla de strings.
 # Respetar esta sintaxis, ya que el CMS dirá que no pasó ningún test si usan otra notación.
+def vuelo_origen (origen: str, vuelos: List[Tuple[str, str]]) -> Tuple[bool, Tuple[str, str]]:
+    res: bool = False
+    vueloDeOrigen: Tuple[str, str] = ('0','0')
+    for vuelo in vuelos:
+        if vuelo[0] == origen:
+            vueloDeOrigen = vuelo
+            res = True
+    return (res, vueloDeOrigen)
+
+
+def ruta (origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
+    ruta: List[Tuple[str, str]] = []
+    while vuelo_origen(origen, vuelos)[0]:
+        ruta.append(vuelo_origen(origen, vuelos)[1])
+        if ruta[len(ruta) - 1][1] != destino:
+            origen = vuelo_origen(origen, vuelos)[1][1]
+        
+        else:
+            origen = 'none'
+
+    if ruta[len(ruta) - 1][1] != destino:
+        ruta = []
+    return ruta
+
+   
+
+
 def sePuedeLlegar(origen: str, destino: str, vuelos: List[Tuple[str, str]]) -> int :
-  # definir esta función
-  return 0
+    if len(ruta(origen, destino, vuelos)) == 0:
+        res = -1
+    else:
+        res = len(ruta(origen, destino, vuelos))
+    return res
 
 if __name__ == '__main__':
   origen = input()
